2453cd4c3222b09a5d5721c58e56a4eb4270d91d
Vipin Sharma <vipin.sharma@twai.com>
2025-10-17 16:29:43 +0530

    Testchange

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WeatherApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "testing"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
